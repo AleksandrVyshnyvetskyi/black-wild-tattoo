a3d244239866cf0594c0d639f5d43d01fb692f3d
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-07-14 22:42:14 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+$('.work__box').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        }
+    ]
+});
