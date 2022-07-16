34f01e98b89e2a10a425fb6a107de596e6d1ad1b
OLEKSANDR VYSHNYVETSKYI <vishniveckijj1994@gmail.com>
2022-07-16 21:28:38 +0300

    burger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,3 +3,14 @@ const menuIsActive = () => {
     hamburgerMenu.classList.toggle('active');
 };
 hamburgerMenu.addEventListener('click', menuIsActive)
+
+document.querySelectorAll('.dropdown-menu__btn').forEach(e => { 
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path;
+        document.querySelectorAll('.dropdown-menu').forEach(e =>{
+            if(hamburgerMenu.classList(`data`)) {
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
+            }
+        })
+    })
+})
