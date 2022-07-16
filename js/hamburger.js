35b01faa42cdb2f2cf0382c1c0840784122d875b
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-07-16 22:48:28 +0300

    burger menu active

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,18 @@
+// BTN
+
 const hamburgerMenu = document.querySelector('.hamburger');
 const menuIsActive = () => {
     hamburgerMenu.classList.toggle('active');
 };
 hamburgerMenu.addEventListener('click', menuIsActive)
 
-document.querySelectorAll('.dropdown-menu__btn').forEach(e => { 
-    e.addEventListener('click', e => {
-        const menu = e.currentTarget.dataset.path;
-        document.querySelectorAll('.dropdown-menu').forEach(e =>{
-            if(hamburgerMenu.classList(`data`)) {
-                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
-            }
-        })
-    })
+
+// MENU
+
+let menuBtn = document.querySelector('.dropdown-menu__btn');
+let menu = document.querySelector('.dropdown-menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('open');
+	menu.classList.toggle('open');
 })
